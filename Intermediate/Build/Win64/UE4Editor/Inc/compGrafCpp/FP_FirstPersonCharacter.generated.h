10b6cacd6890c73375b5b42441a0caef8b4dd9fe
HCassiano <henrique.cassiano.barros@usp.br>
2018-06-28 01:31:44 -0300

    added portal mesh and position, added A and D key behaviour for infinite stairway room

--- Intermediate/Build/Win64/UE4Editor/Inc/compGrafCpp/FP_FirstPersonCharacter.generated.h
+++ Intermediate/Build/Win64/UE4Editor/Inc/compGrafCpp/FP_FirstPersonCharacter.generated.h
@@ -13,9 +13,9 @@ PRAGMA_DISABLE_DEPRECATION_WARNINGS
 #endif
 #define COMPGRAFCPP_FP_FirstPersonCharacter_generated_h
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS_NO_PURE_DECLS
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS_NO_PURE_DECLS \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS_NO_PURE_DECLS
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS_NO_PURE_DECLS \
 private: \
 	static void StaticRegisterNativesAFP_FirstPersonCharacter(); \
 	friend COMPGRAFCPP_API class UClass* Z_Construct_UClass_AFP_FirstPersonCharacter(); \
@@ -25,7 +25,7 @@ public: \
 	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
 
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS \
 private: \
 	static void StaticRegisterNativesAFP_FirstPersonCharacter(); \
 	friend COMPGRAFCPP_API class UClass* Z_Construct_UClass_AFP_FirstPersonCharacter(); \
@@ -35,7 +35,7 @@ public: \
 	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
 
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_STANDARD_CONSTRUCTORS \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_STANDARD_CONSTRUCTORS \
 	/** Standard constructor, called after all reflected properties have been initialized */ \
 	NO_API AFP_FirstPersonCharacter(const FObjectInitializer& ObjectInitializer); \
 	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AFP_FirstPersonCharacter) \
@@ -48,7 +48,7 @@ private: \
 public:
 
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_ENHANCED_CONSTRUCTORS \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_ENHANCED_CONSTRUCTORS \
 private: \
 	/** Private move- and copy-constructors, should never be used */ \
 	NO_API AFP_FirstPersonCharacter(AFP_FirstPersonCharacter&&); \
@@ -59,37 +59,37 @@ DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AFP_FirstPersonCharacter); \
 	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AFP_FirstPersonCharacter)
 
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_PRIVATE_PROPERTY_OFFSET \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_PRIVATE_PROPERTY_OFFSET \
 	FORCEINLINE static uint32 __PPO__Mesh1P() { return STRUCT_OFFSET(AFP_FirstPersonCharacter, Mesh1P); } \
 	FORCEINLINE static uint32 __PPO__FP_Gun() { return STRUCT_OFFSET(AFP_FirstPersonCharacter, FP_Gun); } \
 	FORCEINLINE static uint32 __PPO__FirstPersonCameraComponent() { return STRUCT_OFFSET(AFP_FirstPersonCharacter, FirstPersonCameraComponent); }
 
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_15_PROLOG
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_GENERATED_BODY_LEGACY \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_15_PROLOG
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_GENERATED_BODY_LEGACY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_PRIVATE_PROPERTY_OFFSET \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_STANDARD_CONSTRUCTORS \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_PRIVATE_PROPERTY_OFFSET \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_STANDARD_CONSTRUCTORS \
 public: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
-#define compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_GENERATED_BODY \
+#define compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_GENERATED_BODY \
 PRAGMA_DISABLE_DEPRECATION_WARNINGS \
 public: \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_PRIVATE_PROPERTY_OFFSET \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS_NO_PURE_DECLS \
-	compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_ENHANCED_CONSTRUCTORS \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_PRIVATE_PROPERTY_OFFSET \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_RPC_WRAPPERS_NO_PURE_DECLS \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_INCLASS_NO_PURE_DECLS \
+	compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h_18_ENHANCED_CONSTRUCTORS \
 private: \
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
 
 
 #undef CURRENT_FILE_ID
-#define CURRENT_FILE_ID compgraf_master_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h
+#define CURRENT_FILE_ID compGrafCpp_Source_compGrafCpp_FP_FirstPerson_FP_FirstPersonCharacter_h
 
 
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
